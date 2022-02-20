43c0ac85a6bf71b5d743be09d6934a400f60b16d
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2022-02-20 00:12:13 -0400

    Add pausing and resetting

--- js/clock.mjs
+++ js/clock.mjs
@@ -1,9 +1,66 @@
+const CLOCK_RESOLUTION_TIME = 1000;
+
 export class Clock {
   constructor(callback) {
     this._callback = callback;
+    this._lastTime = 0;
+    this._ellapsedTime = 0;
+    this._interval = null;
+    this._midCycleTimeout = null;
   }
 
   start() {
-    this._interval = setInterval(this._callback, 1000);
+    this._ellapsedTime = 0;
+    this._lastTime = Date.now();
+    this._startInterval();
   }
-};
+
+  pause() {
+    const currentTime = Date.now();
+    this._ellapsedTime = currentTime - this._lastTime;
+    this._clearInterval();
+  }
+
+  reset() {
+    this._clearInterval();
+    this.start();
+  }
+
+  resume() {
+    if (this._ellapsedTime !== 0) {
+      this._startMidCycleTimeout();
+    } else {
+      this._startInterval();
+    }
+  }
+
+  _tick() {
+    this._lastTime = Date.now();
+    this._callback();
+  }
+
+  _startInterval() {
+    this._clearInterval();
+    this._interval = setInterval(() => this._tick(), CLOCK_RESOLUTION_TIME);
+  }
+
+  _clearInterval() {
+    if (this._interval) {
+      clearInterval(this._interval);
+      this._interval = null;
+    }
+    if (this._midCycleTimeout) {
+      clearTimeout(this._midCycleTimeout);
+      this._midCycleTimeout = null;
+    }
+  }
+
+  _startMidCycleTimeout() {
+    const remainingTimeUntilResolution =
+      CLOCK_RESOLUTION_TIME - this._ellapsedTime;
+    this._midCycleTimeout = setTimeout(() => {
+      this._tick();
+      this._startInterval();
+    }, remainingTimeUntilResolution);
+  }
+}
--- js/clock.test.mjs
+++ js/clock.test.mjs
@@ -18,3 +18,38 @@ test('Clock should call function only once after one second', () => {
 
   expect(callback).toHaveBeenCalledTimes(1);
 });
+
+test('Clock should reset its timer when the method reset is called', () => {
+  jest.useFakeTimers();
+  const callback = jest.fn();
+  const clock = new Clock(callback);
+
+  clock.start();
+  jest.advanceTimersByTime(500);
+  clock.reset();
+  jest.advanceTimersByTime(500);
+
+  expect(callback).not.toBeCalled();
+
+  jest.advanceTimersByTime(1000);
+
+  expect(callback).toHaveBeenCalledTimes(1);
+});
+
+test('Clock should pause its timer when the method pause is called', () => {
+  jest.useFakeTimers();
+  const callback = jest.fn();
+  const clock = new Clock(callback);
+
+  clock.start();
+  jest.advanceTimersByTime(500);
+  clock.pause();
+  jest.advanceTimersByTime(500);
+
+  expect(callback).not.toBeCalled();
+
+  clock.resume();
+  jest.advanceTimersByTime(500);
+
+  expect(callback).toHaveBeenCalledTimes(1);
+});
