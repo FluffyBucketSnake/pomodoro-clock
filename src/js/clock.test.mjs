010db86a7a1c2d9e73633686bf0f33adf80aeefb
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2022-02-20 02:09:12 -0400

    Clean Clock test file

--- src/js/clock.test.mjs
+++ src/js/clock.test.mjs
@@ -1,5 +1,6 @@
 import {jest} from '@jest/globals';
 import {ClockStates, Clock} from './clock.mjs';
+import {toMillisecs, toSecs} from './time.mjs';
 
 afterEach(() => {
   jest.useRealTimers();
@@ -7,19 +8,22 @@ afterEach(() => {
 
 it('should call function only once after each second', () => {
   jest.useFakeTimers();
-  const callback = jest.fn();
+  const callback = jest.fn((ellapsedTime) => toSecs(ellapsedTime));
   const clock = new Clock(callback);
 
   clock.run();
 
   expect(clock.state).toBe(ClockStates.Running);
   expect(clock.isRunning).toBe(true);
+  expect(clock.ellapsedTime).toBe(0);
   expect(callback).not.toBeCalled();
 
   for (let i = 1; i < 30; i++) {
-    jest.advanceTimersByTime(1000);
+    jest.advanceTimersByTime(toMillisecs(1));
 
+    expect(clock.ellapsedTime).toBe(toMillisecs(1));
     expect(callback).toHaveBeenCalledTimes(i);
+    expect(callback).toHaveLastReturnedWith(i);
   }
 });
 
@@ -30,11 +34,12 @@ it('should not tick after stop', () => {
 
   clock.run();
   clock.stop();
-  jest.advanceTimersByTime(1000);
+  jest.advanceTimersByTime(toMillisecs(1));
 
   expect(clock.state).toBe(ClockStates.Stopped);
   expect(clock.isStopped).toBe(true);
   expect(clock.isRunning).toBe(false);
+  expect(clock.ellapsedTime).toBe(0);
 });
 
 it('should reset its timer when the method reset is called', () => {
@@ -43,14 +48,16 @@ it('should reset its timer when the method reset is called', () => {
   const clock = new Clock(callback);
 
   clock.run();
-  jest.advanceTimersByTime(500);
+  jest.advanceTimersByTime(toMillisecs(0.5));
   clock.reset();
-  jest.advanceTimersByTime(500);
+  jest.advanceTimersByTime(toMillisecs(0.5));
 
+  expect(clock.ellapsedTime).toBe(toMillisecs(0.5));
   expect(callback).not.toBeCalled();
 
-  jest.advanceTimersByTime(1000);
+  jest.advanceTimersByTime(toMillisecs(0.5));
 
+  expect(clock.ellapsedTime).toBe(toMillisecs(1));
   expect(callback).toHaveBeenCalledTimes(1);
 });
 
@@ -60,19 +67,21 @@ it('should pause its timer when the method pause is called', () => {
   const clock = new Clock(callback);
 
   clock.run();
-  jest.advanceTimersByTime(500);
+  jest.advanceTimersByTime(toMillisecs(0.5));
   clock.pause();
-  jest.advanceTimersByTime(500);
+  jest.advanceTimersByTime(toMillisecs(0.5));
 
   expect(clock.state).toBe(ClockStates.Paused);
   expect(clock.isRunning).toBe(false);
   expect(clock.isStopped).toBe(false);
   expect(clock.isPaused).toBe(true);
+  expect(clock.ellapsedTime).toBe(toMillisecs(0.5));
   expect(callback).not.toBeCalled();
 
   clock.run();
-  jest.advanceTimersByTime(500);
+  jest.advanceTimersByTime(toMillisecs(0.5));
 
+  expect(clock.ellapsedTime).toBe(toMillisecs(1.0));
   expect(callback).toHaveBeenCalledTimes(1);
 });
 
@@ -84,9 +93,10 @@ it('should compensate for interval and timeout inaccuracy', () => {
   const clock = new Clock(callback);
 
   clock.run();
-  trueTime = 4 * 1000;
-  jest.advanceTimersByTime(1000);
+  trueTime = toMillisecs(4.3);
+  jest.advanceTimersByTime(toMillisecs(1));
 
+  expect(clock.ellapsedTime).toBe(toMillisecs(4.3));
   expect(callback).toHaveBeenCalledTimes(4);
 });
 
@@ -96,7 +106,7 @@ it('should not tick if it is not running', () => {
   const clock = new Clock(callback);
 
   clock.reset();
-  jest.advanceTimersByTime(1000);
+  jest.advanceTimersByTime(toMillisecs(1));
 
   expect(callback).not.toBeCalled();
 });
